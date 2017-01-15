33df5aa9adb49c95a81242da5d353891bf6d29b5
liuqiuchen <2016968116@qq.com>
2017-01-15 21:11:50 +0800

    commit

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html', {'hello': 'Hello, Blog!'})
+    return render(request, 'blog/index.html', {'hello': 'Hello, Blog!'})
--- blog2/views.py
+++ blog2/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html', {'hello': 'Hello, Blog!'})
+    return render(request, 'blog2/index.html', {'hello': 'Hello, Blog!'})
