7c9e28bac43056fb0f5b3f297ab37980053e11f6
liuqiuchen <2016968116@qq.com>
2017-01-21 15:22:58 +0800

    commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from blog import models
 
-# Register your models here.
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time', )
+
+admin.site.register(models.Article, ArticleAdmin)
